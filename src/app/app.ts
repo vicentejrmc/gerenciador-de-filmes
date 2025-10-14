938c90a406436a0c92b920de1cf9a8845894e0f8
Vicente Junior <vicente_pc@outlook.com>
2025-10-14 09:56:42 -0300

    ajuste de arquivo banner-principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { BannerPrincipal } from './components/banner-principal/banner-principal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, BannerPrincipal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('gerenciador-de-filmes');
 }
+
---
+++ src/app/components/banner-principal/banner-principal.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-principal',
+  imports: [],
+  templateUrl: './banner-principal.html',
+})
+export class BannerPrincipal {}
