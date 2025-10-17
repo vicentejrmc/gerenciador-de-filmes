e721cfe18fcddb1d78d29f546cced753cb29234d
Vicente Junior <vicente_pc@outlook.com>
2025-10-17 11:11:44 -0300

    ajustado routerLink - refatorada front-end

--- src/app/services/midia-service.ts
+++ src/app/services/midia-service.ts
@@ -13,30 +13,42 @@ export class MidiaService {
   private readonly urlBase: string = 'https://api.themoviedb.org/3';
 
   public selecionarMidiasPopulares(tipo: TipoMidia) {
-    const tipoSelecionado = tipo === 'filme' ? 'movie' : 'tv';
+    const tipoSelecionado = this.converterTipoParaApi(tipo);
     const url = `${this.urlBase}/${tipoSelecionado}/popular?language=pt-BR`;
-    return this.buscarMidias(url);
+    return this.buscarMidias(url, tipo);
   }
 
     public selecionarMidiasMaisVotadas(tipo: TipoMidia) {
-      const tipoSelecionado = tipo === 'filme' ? 'movie' : 'tv';
+      const tipoSelecionado = this.converterTipoParaApi(tipo);
       const url = `${this.urlBase}/${tipoSelecionado}/top_rated?language=pt-BR`;
-      return this.buscarMidias(url);
+      return this.buscarMidias(url, tipo);
   }
 
   public selecionarFilmesEmCartaz() {
     const url = `${this.urlBase}/movie/now_playing?language=pt-BR`;
-    return this.buscarMidias(url);
+    return this.buscarMidias(url, TipoMidia.Filme);
   }
 
   //Método para buscar midias na api.
-  private buscarMidias(url: string): Observable<MidiaApiResponse>{
+  private buscarMidias(url: string, tipoMidia: TipoMidia): Observable<MidiaApiResponse> {
     return this.http.get<MidiaApiResponse>(url, {
-      headers: {Authorization: environment.apiKey},
-    }).pipe(map((x) => ({...x, results: x.results.map((y) => ({...y,
-        poster_path: 'https://image.tmdb.org/t/p/w500' + y.poster_path,
-        backdrop_path: 'https://image.tmdb.org/t/p/original' + y.backdrop_path,
-    }))})));
+      headers: { Authorization: environment.apiKey },
+    }).pipe(
+      map((x) => ({
+        ...x,
+        type: tipoMidia,
+        results: x.results.map((y) => ({
+          ...y,
+          poster_path: 'https://image.tmdb.org/t/p/w500' + y.poster_path,
+          backdrop_path: 'https://image.tmdb.org/t/p/original' + y.backdrop_path,
+        })),
+      }))
+    );
+  }
+
+  //converter tipo.
+  private converterTipoParaApi(tipo: TipoMidia): 'movie' | 'tv' {
+    return tipo === TipoMidia.Filme ? 'movie' : 'tv';
   }
 }
 
